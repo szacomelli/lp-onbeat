97fa70db9bc2c09f7f21b1f325133335c9620c47
stephanycasali <sy@Fenix>
2024-11-05 18:50:08 -0300

    welcome_screen

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from threading import Timer as tm
 import notes as nt, keyfields as kf, actualround as ar, music as ms
+import screen_classes as sc
 
 pg.mixer.pre_init(44100, channels=2, buffer=512)
 pg.mixer.init()
@@ -31,6 +32,7 @@ print(pg.mixer.get_init())
 
 
 running = True
+running = sc.Welcome_screen((SCREEN_WIDTH,SCREEN_HEIGHT),running, clock).run()
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
