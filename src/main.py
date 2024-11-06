bdd468a383edf6fce97bfba266258daf369aa8b1
unknown <stephanycasali@gmail.com>
2024-11-06 08:21:16 -0300

    fixing directories (again)

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ key_fields = [kf.KeyField(100, 400, (255, 0, 0), (220, 0, 0), pg.K_s),
               kf.KeyField(400, 400, (255, 255, 0), (220, 220, 0), pg.K_l)]
 
 
-musica = ms.ItaloMusic("../FullScores/Retro Scores/Ove Melaa - Italo Unlimited.mp3", key_fields)
+musica = ms.ItaloMusic("./FullScores/Retro Scores/Ove Melaa - Italo Unlimited.mp3", key_fields)
 round = ar.ActualRound(key_fields, musica)
 
  #+ notes_refrao.copy() + notes_refrao.copy() + notes_refrao.copy()
